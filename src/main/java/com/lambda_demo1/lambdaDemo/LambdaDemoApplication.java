6be22c869f3dd8223ee3f5a6f3a73b491c0322d2
Priyansh Singh <n200@clarica.com>
2024-10-15 17:46:51 +0530

    initialized repo

---
+++ src/main/java/com/lambda_demo1/lambdaDemo/LambdaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lambda_demo1.lambdaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lambda_demo1/lambdaDemo/LambdaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda_demo1.lambdaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
